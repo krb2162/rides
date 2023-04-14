77a34f80ebdb93a7f8510700b6fdbb3428412e8d
Katie <ktbrady7@gmail.com>
2023-04-14 16:21:47 -0700

    add in future scope tests

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe Ride, type: :model do
     # let(:address) { "50123 Blueberry Lane, Santa Cruz, CA" }
     # let(:driver) { create(:driver, home_address: address) }
-    let(:ride) { create(:ride, pick_up_at: Time.zone.now ) }
+    let(:ride) { create(:ride, pick_up_at: Time.zone.tomorrow ) }
+    let(:ride_future_2) { create(:ride, pick_up_at: Time.zone.tomorrow ) }
+    let(:ride_past) { create(:ride, pick_up_at: Time.zone.yesterday) }
 
 	# describe "relationships" do
     #     it { should belong_to(:driver).optional }
@@ -21,5 +23,29 @@ RSpec.describe Ride, type: :model do
                 expect(ride).to be_valid
             end
         end
+    end
+
+    describe "scopes" do
+        context ".in_future" do
+            it "only includes rides that have future pick up times" do
+                expect(Ride.in_future).to include(ride)
+                expect(Ride.in_future).to include(ride_future_2)
+            end
+
+            it "excludes rides that have past pick up times" do
+                expect(Ride.in_future).to_not include(ride_past)
+            end
+        end
+
+        context ".available" do
+            it "includes rides that don't have a driver" do
+            end
+
+            it "excludes rides that do have a driver" do
+            end
+        end
+    end
+
+    describe "methods" do
     end
 end

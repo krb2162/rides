37e9babc5c07763b46bc96a017bdca313e2e3b67
Katie <ktbrady7@gmail.com>
2023-04-13 20:38:27 -0700

    add driver factory and add driver to rides factory

---
+++ spec/factories/drivers.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+	factory :driver do
+		home_address { "28782 Camino Capistrano, Laguna Niguel, CA 92677" }
+	end
+end
--- spec/factories/rides.rb
+++ spec/factories/rides.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
 	factory :ride do
         route
+        driver
 		pick_up_at { Time.zone.now }
 	end
 end

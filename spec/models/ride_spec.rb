034bda77d117db9a6fffd345f46098e3ccd119f7
Katie <ktbrady7@gmail.com>
2023-04-14 16:43:25 -0700

    rspec available rides scope

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
-    # let(:address) { "50123 Blueberry Lane, Santa Cruz, CA" }
-    # let(:driver) { create(:driver, home_address: address) }
+    let(:address) { "50123 Blueberry Lane, Santa Cruz, CA" }
+    let(:driver) { create(:driver, home_address: address) }
+    
     let(:ride) { create(:ride, pick_up_at: Time.zone.tomorrow ) }
-    let(:ride_future_2) { create(:ride, pick_up_at: Time.zone.tomorrow ) }
-    let(:ride_past) { create(:ride, pick_up_at: Time.zone.yesterday) }
+    let(:ride_2) { create(:ride, pick_up_at: Time.zone.tomorrow) }
+    let(:ride_3) { create(:ride, pick_up_at: Time.zone.yesterday) }
 
 	# describe "relationships" do
     #     it { should belong_to(:driver).optional }
@@ -27,21 +28,33 @@ RSpec.describe Ride, type: :model do
 
     describe "scopes" do
         context ".in_future" do
+
             it "only includes rides that have future pick up times" do
                 expect(Ride.in_future).to include(ride)
-                expect(Ride.in_future).to include(ride_future_2)
+                expect(Ride.in_future).to include(ride_2)
             end
 
             it "excludes rides that have past pick up times" do
-                expect(Ride.in_future).to_not include(ride_past)
+                expect(Ride.in_future).to_not include(ride_3)
             end
         end
 
         context ".available" do
             it "includes rides that don't have a driver" do
+                ride.update!(driver_id: nil)
+                ride_2.update!(driver_id: driver.id)
+                ride_3.update!(driver_id: driver.id)
+                
+                expect(Ride.available).to include(ride)
             end
 
             it "excludes rides that do have a driver" do
+                ride.update!(driver_id: nil)
+                ride_2.update!(driver_id: driver.id)
+                ride_3.update!(driver_id: driver.id)
+                
+                expect(Ride.available).to_not include(ride_2)
+                expect(Ride.available).to_not include(ride_3)
             end
         end
     end

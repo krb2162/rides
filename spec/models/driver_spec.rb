021c01d4d9890014824d8047bf1e375536e74062
Katie <ktbrady7@gmail.com>
2023-04-14 19:33:51 -0700

    refactor driver model specs to use shoulda

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -1,26 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
-    let(:address) { "50123 Blueberry Lane, Santa Cruz, CA" }
-    let(:driver) { create(:driver, home_address: address) }
-    let!(:ride) { create(:ride, driver_id: driver.id) }
-
 	describe "relationships" do
-        it "should have many rides when associated to rides" do
-            expect(driver.rides.count).to eq(1)
-        end
+        it { should have_many(:rides) }
 	end
 
     describe "validations" do
-        context "home address" do
-            it "is not valid without the presence of home address" do
-                driver.home_address = nil
-                expect(driver).to_not be_valid
-            end
-            
-            it "is valid with presence of home address" do
-                expect(driver).to be_valid
-            end
-        end
+	    it { should validate_presence_of(:home_address) }
     end
 end
